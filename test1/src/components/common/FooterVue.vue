8aa0911d991c25fb0ac6edc2919209d9f815fa31
박세희 <kjudi12@cu.ac.kr>
2022-11-22 21:45:15 +0900

    11.22

--- test1/src/components/common/FooterVue.vue
+++ test1/src/components/common/FooterVue.vue
@@ -12,5 +12,5 @@ export default {
 
 <style scoped>
 footer{border-top: 1px solid #35495e; text-align: center;
- font-size: 16px; color: #41b883; margin: 100px 0 0 0;}
+ font-size: 16px; color: white; background-color: black; margin: 100px 0 0 0;}
 </style>
--- test1/src/components/common/ImageListVue.vue
+++ test1/src/components/common/ImageListVue.vue
@@ -1,16 +1,23 @@
 <template>
-    <div class="imagelist">
-        <h1>ImageList</h1>
-        
-        
+    <div>
+        <a href="#" v-for="i in imagelist" :key="i" class="imagelist">{{i}}</a><br><br><br><br><br><br><br>
+        <a href="#" v-for="i in imagelist1" :key="i" class="imagelist">{{i}}</a><br><br><br><br><br><br><br>
+        <a href="#" v-for="i in imagelist2" :key="i" class="imagelist">{{i}}</a>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            imagelist : [1,2,3],
+            imagelist1 : [4,5,6],
+            imagelist2 : [7,8,9]
+        }
+    }
 }
 </script>
 <style scoped>
-div.imagelist {width: 200px; height: 200px; position: relative; left: 240px; bottom: 150px; border: 1px solid black;}
+.imagelist {padding: 45px 75px; position: relative; border: 1px solid black;
+bottom: 110px; left: 100px; font-size: 50px;}
 </style>
